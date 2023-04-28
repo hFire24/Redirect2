6b4dd9ce93ea0bcd35f5c31a6f5a68a920b9393d
hFire24 <51500563+hFire24@users.noreply.github.com>
2023-04-28 14:29:33 -0400

    Added spacing to the buttons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -40,5 +40,5 @@
 <template>
   <h1>{{ messageText }}</h1>
   <div v-if="redirectedFromWebsite" class="space">You tried to access {{ website }} {{ numTimes }} time<span v-if="numTimes !== 1">s</span> today.</div>
-  <div>{{ question }} <button class="green" @click="acceptSomething">Yes</button><button class="red" @click="rejectSomething" :disabled="rejectId === 11">No</button></div>
+  <div>{{ question }}<br><button class="green" @click="acceptSomething">Yes</button><button class="red" @click="rejectSomething" :disabled="rejectId === 11">No</button></div>
 </template>
